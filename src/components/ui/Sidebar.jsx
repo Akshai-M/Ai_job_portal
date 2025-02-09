fe1ad087964796420eeb3f9e9d59d0dce0d87494
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-02-09 13:04:55 +0530

    Sidebar

---
+++ src/components/ui/Sidebar.jsx
@@ -0,0 +1,15 @@
+import { cn } from "../../lib/utils";
+import { Link } from "react-router-dom";
+import React, { useState, createContext, useContext } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { IconMenu2, IconX } from "@tabler/icons-react";
+
+const SidebarContext = createContext();
+
+export const useSidebar = () => {
+  const context = useContext(SidebarContext);
+  if (!context) {
+    throw new Error("useSidebar must be used within a SidebarProvider");
+  }
+  return context;
+};
