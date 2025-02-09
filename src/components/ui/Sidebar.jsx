58933d946bc63f195c8d8f1abf2c3541b3db245f
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-02-09 13:06:18 +0530

    Sidebar

--- src/components/ui/Sidebar.jsx
+++ src/components/ui/Sidebar.jsx
@@ -79,7 +79,31 @@ export const MobileSidebar = ({ className, children, ...props }) => {
           onClick={() => setOpen(!open)}
         />
       </div>
-      
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            initial={{ x: "-100%", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ x: "-100%", opacity: 0 }}
+            transition={{
+              duration: 0.3,
+              ease: "easeInOut",
+            }}
+            className={cn(
+              "fixed h-full w-full inset-0 bg-white dark:bg-neutral-900 p-10 z-[100] flex flex-col justify-between",
+              className
+            )}
+          >
+            <div
+              className="absolute right-10 top-10 z-50 text-neutral-800 dark:text-neutral-200"
+              onClick={() => setOpen(!open)}
+            >
+              <IconX />
+            </div>
+            {children}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
