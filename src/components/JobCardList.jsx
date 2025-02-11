6f8b90b117e827388f01ff5ceafd801d0ab57e3d
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-02-11 13:30:23 +0530

    JobCardList

--- src/components/JobCardList.jsx
+++ src/components/JobCardList.jsx
@@ -3,7 +3,13 @@ import JobCard from './JobCard';
 
 const JobCardList = ({ jobs }) => {
   return (
-    
+    <div className=" ">
+      <div className="">
+        {jobs.map((job) => (
+          <JobCard key={job.id} job={job} />
+        ))}
+      </div>
+    </div>
   );
 };
 
