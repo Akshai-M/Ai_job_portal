03fd5c479a1b2902eaf0f62bbf7c33c52076e77a
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-02-12 13:26:32 +0530

    JobCard

---
+++ src/components/JobCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const JobCard = ({ job }) => {
+  
+
+  return (
+    <div
+      className=" bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-transform duration-300 border border-gray-200"
+    >
+      
+      
+    </div>
+  );
+};
+
+export default JobCard;
