03f85616303bb7fbae6bfcea779f7a1e872dfba8
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-02-14 14:38:16 +0530

    Newthread

---
+++ src/components/NewThread.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewThread() {
+  return (
+    <div className="absolute top-7 w-64 lg:w-full max-w-lg mx-auto p-6 bg-indigo-200 rounded-lg border border-black">
+    
+    </div>
+  );
+}
+
+export default NewThread;
