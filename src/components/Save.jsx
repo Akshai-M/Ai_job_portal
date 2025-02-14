8765d531f60a6da9a4618ea3254ef9e04bb8515c
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-02-14 14:40:37 +0530

    Save

--- src/components/Save.jsx
+++ src/components/Save.jsx
@@ -12,8 +12,23 @@ const Bookmark = () => {
           <h1 className="text-xl font-semibold text-gray-800">
             No Saved Jobs Yet
           </h1>
-          
+          <p className="text-gray-600 mt-2">
+            Jobs you find during your job search threads will appear here for
+            easy organization and tracking.
+          </p>
+          <p className="text-gray-600">
+            Start a new job search thread to begin discovering and saving
+            relevant positions.
+          </p>
+          <p className="text-gray-400 mt-4">Need more info on Threads?</p>
+
+          <div className="flex justify-center mt-5">
+            <p className="border-l-4 rounded-lg border-blue-600 w-fit py-2 text-black pl-2">
+              We've got you covered →
+            </p>
+          </div>
         </div>
+       
       </div>
       
     </>
