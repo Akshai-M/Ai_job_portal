2ac295edf1b87d34811ef6ab2f4be13b1a0e999b
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-02-14 14:39:28 +0530

    Save

---
+++ src/components/Save.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./ui/Button";
+import { Edit, Trash2 } from "lucide-react";
+
+const Bookmark = () => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default Bookmark;
