05ef98ea41e8b459371c7395d802230d0a447664
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-02-14 14:40:22 +0530

    Save

--- src/components/Save.jsx
+++ src/components/Save.jsx
@@ -5,6 +5,16 @@ import { Edit, Trash2 } from "lucide-react";
 const Bookmark = () => {
   return (
     <>
+      <div className="grid items-center justify-center ">
+        {" "}
+
+        <div className="max-w-2xl w-full text-center p-6 bg-white rounded-lg shadow-lg">
+          <h1 className="text-xl font-semibold text-gray-800">
+            No Saved Jobs Yet
+          </h1>
+          
+        </div>
+      </div>
       
     </>
   );
