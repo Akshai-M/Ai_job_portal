f068635c99ddcb87dd364864cb5ec0a24c58f260
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-03-03 23:40:46 +0530

    ShowJobs

---
+++ src/components/ShowJobs.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import JobCard from "./JobCard";
+import LOGO from '@/app/favicon.ico';
+
+
+function ShowJobs() {
+  const jobCards = [
+    {
+      id: 1,
+      title: "Senior Frontend Developer",
+      company: "Google",
+      logo: LOGO,
+      experience: "3+ years",
+      salary: "15-20",
+      timeDuration: "Full-time",
+      location: "Mountain View, CA",
+      startsIn: "3 days",
+      skills: ["CSS", "React", "JavaScript"],
+      description:
+        "Lead the development of cutting-edge web applications and collaborate with teams.",
+      applyUrl: "https://www.microsoft.com",
+    },
+    
+  ];
+
+  return (
+    <div className="max-w-7xl mx-5 border absolute top-[1rem] bg-[#e4e4e7] border-gray-500 lg:rounded-t-none border-t-gray-300 rounded-lg  lg:mt-0">
+      <div className="sm:max-h-[535px]  max-h-[27.5rem] overflow-y-auto overflow-x-hidden p-4 space-y-4 scrollbar-hide ">
+        {jobCards.map((job) => (
+          <JobCard key={job.id} job={job} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ShowJobs;
