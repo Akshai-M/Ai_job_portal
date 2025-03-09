9e47e7269ad6fc4650aff7afc143d91ffc6da3fa
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-03-09 13:06:30 +0530

    Chatbot

---
+++ src/app/Chatbot.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Chatbot() {
+  return (
+    <div className='text-3xl font-bold text-center text-black'>
+      Chatbot
+    </div>
+  )
+}
+
+export default Chatbot
