f4bc67f90bde871647353c9c35028b7a423f7c2c
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-03-16 12:16:46 +0530

    page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
 import Dashboard from "./Dashboard";
-
+import Myapp from "./Myapp";
 export default function Page() {
   return (
     <>
-    <Dashboard />
+    {/* <Dashboard /> */}
+    <Myapp/>
     </>
 )
 }
